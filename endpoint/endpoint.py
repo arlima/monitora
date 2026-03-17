72d9dfc314b86279a3f9c4071793c7f323b95823
Adriano R. de Lima <adriano.lima@arlima.com.br>
2026-03-17 18:51:47 -0300

    better endpoint error handling

--- endpoint/endpoint.py
+++ endpoint/endpoint.py
@@ -9,12 +9,12 @@ with open("/etc/monitora/endpoint.yml", 'r', encoding="utf8") as config_file:
 
 def main():
     """ main is the main function """
+    auth = HTTPBasicAuth(config["USER"], config["PWD"])
     while True:
         try:
-            auth = HTTPBasicAuth(config["USER"], config["PWD"])
-            requests.post(config["SERVER"], data = {'host':config["HOST"]}, auth=auth)
-        except requests.exceptions.RequestException:
-            print("Server error")
+            requests.post(config["SERVER"], data={'host': config["HOST"]}, auth=auth, timeout=10)
+        except requests.exceptions.RequestException as e:
+            print(f"Server error: {e}")
         time.sleep(config["INTERVAL"])
 
 if __name__ == "__main__":
